fdf88aba28ee303f60865c00debf8fa8b320bc9d
Aslam <aslamcj@gmail.com>
2018-11-23 18:40:49 +0530

    Create objectConstructor.js

---
+++ objectConstructor.js
@@ -0,0 +1,29 @@
+function TestObj(fname, age) {
+  this.firstName = fname;
+  this.age = age;  
+}
+
+TestObj.prototype.displayNameAndAge = function() {
+  this.age = this.age*10;
+   console.log("User Name: "+this.firstName+" User Age: "+this.age);
+}
+
+var obj1 = new TestObj("aslam", 30);
+var obj2 = new TestObj("zahid", 4);
+
+obj1.age  // prints 30
+obj2.age  // prints 4
+
+obj1.displayNameAndAge(); //
+obj2.displayNameAndAge(); //
+
+
+obj1.age  // prints 300
+obj2.age  // prints 12
+
+obj2.__proto__.constructor    // Displays the current function constructor object which it declared
+                              // TestObj()
+                              
+obj2.__proto__.constructor === TestObj.prototype.constructor  // compares and display wether both are same
+
+
